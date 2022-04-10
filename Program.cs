a3af2a94ee354c420db09c5f3205c91bac985b21
= <=>
2022-04-10 19:51:07 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿double j, m, c, i,t;
+
+Console.Write("Capital [c] (R$).......:");
+c = Convert.ToDouble (Console.ReadLine());
+
+Console.Write("Taxa de juros [i] (%)..:");
+i = Convert.ToDouble (Console.ReadLine());
+
+Console.Write("Tempo [t] (meses)......:");
+t = Convert.ToDouble (Console.ReadLine());
+
+//Calcula Juros Simples 
+
+j = c * (i / 100) * t;
+
+//Calcula o montante
+
+m = c + j;
+
+Console.WriteLine($"\nJuros (R$).....: {j:N2}");
+Console.WriteLine($"Montante (R$)..: {m:N2}");
